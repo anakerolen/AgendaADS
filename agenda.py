313f44dfc652872a43a03c1772aea4fb25667b29
anakerolen <32919192+anakerolen@users.noreply.github.com>
2018-03-07 19:41:54 -0300

    Update agenda.py

--- agenda.py
+++ agenda.py
@@ -1,19 +1,28 @@
-#Agenda Telefonica
-import funcoes
+#Agenda
 
-funcoes.bemvindo()
+#Definições variáveis
 
-#Opcoes do Usuario
-opcao = int(raw_input())
-print("Seleccionaste", opcao)
+opcao = 0
 
 
 #Estrutura de controle
-if opcao == 1:
-	funcoes.adicionar()
-elif opcao == 2:
-	funcoes.listar()
-else:
-	funcoes.falha()
+
+while opcao != 9:
+    opcao = int(input('\n\nSelecione uma opção (1, 2, 4, 5 ou 9 para sair): '))
+
+    if opcao == 1:
+        print('\n\nOpcao 1')        #Inserir funções para executar opção 1
+
+    elif opcao == 2:
+        print('\n\nOpcao 2')        #Inserir funções para executar opção 2
+
+    elif opcao == 4:
+        print('\n\nOpcao 4')        #Inserir funções para executar opção 4
+
+    elif opcao == 5:
+        print('\n\nOpcao 5')        #Inserir funções para executar opção 5
 
 
+#Opção 9 selecionada
+        
+print('\n\nOpcao 9 - Sair (Fim do programa)')
